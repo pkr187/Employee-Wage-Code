916917e12fed8e44a00ad502be87cd5fb0826dba
pkr187 <pankajroman187@gmail.com>
2022-12-21 10:08:46 +0530

    [pankaj] Add:EmpPresentOrAbsent

---
+++ EmpWageCode/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace EmpWageCode
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //constants
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            //Computation
+            int empcheck = random.Next(0, 2);
+            if (empcheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
