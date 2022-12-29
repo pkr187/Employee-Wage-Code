e9cb65033c4ae8da25985910fd12563c115cf587
pkr187 <pankajroman187@gmail.com>
2022-12-29 12:19:09 +0530

    [pankaj] Add:AbilityToManageEmpWageForMultipleComp

--- EmpWageCode/Program.cs
+++ EmpWageCode/Program.cs
@@ -2,16 +2,14 @@
 
 namespace AbilitytoCompute
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
-            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 20, 2, 10);
-            dMart.ComputeEmpWage();
-            Console.WriteLine(dMart.toString());
-            reliance.ComputeEmpWage();
-            Console.WriteLine(reliance.toString());
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 50);
+            empWageBuilder.addCompanyEmpWage("Reliance", 100, 4, 20);
+            empWageBuilder.computeEmpWage();
         }
     }
 }
---
+++ EmpWageCode/companyEmpWage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageCode
+{
+    internal class companyEmpWage
+    {
+        public String Company;
+        public int empRatePerHour;
+        public int numofWorkingDays;
+        public int maxHoursPerMonth;
+        public int totalEmpWage;
+        public companyEmpWage(string company, int empRatePerHour, int numofWorking, int maxHoursPerMonth)
+        {
+            this.Company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numofWorkingDays = numofWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+        public void setTotalEmpWage(int totalEmpWage)
+        {
+            this.totalEmpWage = totalEmpWage;
+        }
+        public string tostring()
+        {
+            return "Total Emp Wage for Company:" + this.Company + "is:" + this.totalEmpWage;
+        }
+    }
+}
+    
+
